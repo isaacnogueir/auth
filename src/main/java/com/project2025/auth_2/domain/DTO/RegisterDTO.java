789409d08bfa6924badcb74aacd644f271cf6416
Isaac <isaac.nogueira@icloud.com>
2025-07-15 16:35:11 -0300

    chore: remover todos os arquivos do repositório remoto

--- src/main/java/com/project2025/auth_2/domain/DTO/RegisterDTO.java
+++
@@ -1,6 +0,0 @@
-package com.project2025.auth_2.domain.DTO;
-
-import com.project2025.auth_2.domain.Role;
-
-public record RegisterDTO(String login, String password, Role role){
-}
--- src/main/java/com/project2025/auth_2/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.project2025.auth_2.domain;
-
-public enum Role {
-    USER, ADMIN
-}
